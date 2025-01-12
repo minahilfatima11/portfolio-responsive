a565cc96839d63c88dcfe8eb9f7ba8753d90e64c
Minahil Fatima <fminahil16343@gmail.com>
2025-01-11 17:46:03 -0800

    updated

--- lib/views/home-views/home-view.dart
+++ lib/views/home-views/home-view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:web85/controller/widgets/responsivelayout.dart';
 import 'package:web85/views/home-views/desktop-view/home-view.dart';
 import 'package:web85/views/home-views/mobile-view/homeview-mobile.dart';
-import 'package:web85/views/home-views/tablet-view/tablet-view-home.dart';
+
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -14,7 +14,7 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   final GlobalKey<State<HomeViewDesktop>> _desktopKey = GlobalKey();
   final GlobalKey<State<HomeViewMobile>> _mobileKey = GlobalKey();
-  final GlobalKey<State<HomeViewTablet>> _tabletKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return ResponsiveLayout(
@@ -24,9 +24,7 @@ class _HomeViewState extends State<HomeView> {
       mobile: HomeViewMobile(
         key: _mobileKey,
       ),
-      tablet: HomeViewTablet(
-        key: _tabletKey,
-      ),
+
     );
   }
 }
